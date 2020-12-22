e43d96bc9afc4d4194094489f5ec07baa3160b22
Matt Kukla <43801193+matt-kukla@users.noreply.github.com>
2020-12-22 16:25:42 -0500

    initial commit

--- src/tofn.ml
+++ src/tofn.ml
@@ -1,1 +1,70 @@
+exception OFN_type_mismatch
+exception Improper_OFN
+
+type family = Trapezoidal | Gaussian | Exponential 
+
+type tofn = {
+    ofn_type : family; 
+    au: float; 
+    bu : float;
+    ad : float; 
+    bd : float; 
+}
+
+let sametype x y = x.ofn_type == y.ofn_type
+
+let tuplemap f x y = {  
+    ofn_type = x.ofn_type; 
+    au = f x.au y.au; 
+    bu = f x.bu y.bu;
+    ad =  f x.ad y.ad; 
+    bd = f x.bd y.bd;
+}
+
+(* Arithmetic operations *)
+let (|+|) x y = 
+    if sametype x y then tuplemap (+.) x y else raise OFN_type_mismatch
+
+let (|-|) x y = 
+     if sametype x y then tuplemap (-.) x y else raise OFN_type_mismatch
+
+let (|*|) x y = 
+     if sametype x y then tuplemap ( *. ) x y else raise OFN_type_mismatch
+
+let (|/|) x y = 
+    if y.au == 0. || y.bu == 0. || y.ad == 0. || y.bd == 0. then raise Division_by_zero 
+    else tuplemap (/.) x y
+
+let is_proper x = (x.au <= x.bu  && x.bu <= x.ad && x.ad <= x.bd) ||
+                  (x.au >= x.bu  && x.bu >= x.ad && x.ad >= x.bd) ||
+                  (x.au = 0. && x.ad = 0. && x.bu <> 0. && x.bd <> 0.)
+
+let is_increasing x = (is_proper x) && (x.au > 0.) && (x.ad < 0.)
+
+let is_decreasing x = (is_proper x) && (x.au < 0.) && (x.ad > 0.)
+
+let membership x = 
+    if not (is_proper x) then raise Improper_OFN else
+    let mem f fi y =
+        if is_increasing x then
+            if y >= (x.au *. (f 0.) +. x.bu) && y < (x.au *. (f 1.) +. x.bu) 
+                then fi ((y -. x.bu) /. x.au)
+            else if y > (x.ad *. (f 1.) +. x.bd) &&  y <= (x.au *. (f 0.) +. x.bu)
+                then fi ((y -. x.bd) /. x.ad)
+            else 1. 
+
+        else if (is_decreasing x) then
+            if y >= (x.au *. (f 0.) +. x.bu) && y < (x.au *. (f 1.) +. x.bu)
+                then fi ((y -. x.bd) /. x.ad)
+            else if y > (x.ad *. (f 1.) +. x.bd) &&  y <= (x.au *. (f 0.) +. x.bu)
+                then fi ((y -. x.bu) /. x.au)
+            else 1.
+        
+        else raise Improper_OFN
+
+    in 
+        match x.ofn_type with
+            | Trapezoidal -> (mem (fun z ->  z) (fun z -> z))  
+            | Gaussian -> (mem (fun z -> -2. *. log z) (fun z -> -0.5 *. (z**2.))) 
+            | Exponential -> (mem (fun z -> exp z) (fun z -> log z))
 
---
+++ src/tofn.mli
@@ -0,0 +1,56 @@
+(** Typed ordered fuzzy numbers (OFNs) and associated operations, following 
+{{: https://arxiv.org/abs/2010.07764}"Rings of Typed Ordered Fuzzy
+Numbers."}*)
+
+(** @author Matthew Kukla *)
+
+(** Incompatible OFN type families. *)
+exception OFN_type_mismatch
+
+(** An OFN is improper when it has no membership function. *)
+exception Improper_OFN
+
+(** Supported families are trapezoidal, Gaussian, and exponential. *)
+type family = Trapezoidal | Gaussian | Exponential
+
+(** OFN with type [family]  and essential tuple (a{_u}, b{_u}, a{_d}, b{_d}).
+Components of the tuple are represented as individual fields (as opposed to
+an element of [float * float * float * float]) for ease-of-access.*)
+type tofn = {
+  ofn_type : family;
+  au : float;
+  bu : float;
+  ad : float;
+  bd : float;
+}
+
+(** Determine if two given OFNs are of the same type. *)
+val sametype : tofn -> tofn -> bool
+
+(** Application of a binary operator to the essential tuples of two OFNs. *)
+val tuplemap : (float -> float -> float) -> tofn -> tofn -> tofn
+
+(** OFN addition. *)
+val ( |+| ) : tofn -> tofn -> tofn
+
+(** OFN Subtraction. *)
+val ( |-| ) : tofn -> tofn -> tofn
+
+(** OFN multiplication. *)
+val ( |*| ) : tofn -> tofn -> tofn
+
+(** OFN division *)
+val ( |/| ) : tofn -> tofn -> tofn
+
+(** Check if an OFN is proper. *)
+val is_proper : tofn -> bool
+
+(** Determine if an OFN is increasing. *)
+val is_increasing : tofn -> bool
+
+(** Determine if an OFN is decreasing. *)
+val is_decreasing : tofn -> bool
+
+(** The membership function associated to an OFN.  Raises [Imroper_OFN] if the OFN 
+is improper. *)
+val membership : tofn -> float -> float
